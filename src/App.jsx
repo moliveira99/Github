c9740976c05e08689826c3b493821139c48bba09
moliveira99 <143028655+moliveira99@users.noreply.github.com>
2024-03-16 14:13:52 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GitHubProfiles from './GitHubProfiles';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GitHubProfiles />
+    </div>
+  );
+}
+
+export default App;
